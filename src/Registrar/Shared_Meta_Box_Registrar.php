68b302fcb889a756fe427ee597eb4a5d3a87c850
gin0115 <glynn.quelch@gmail.com>
2021-10-26 12:23:15 +0100

    Shared meta box controller adde with tests

---
+++ src/Registrar/Shared_Meta_Box_Registrar.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Registration Registrar for Shared Meta Boxes.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Glynn Quelch <glynn.quelch@gmail.com>
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @package PinkCrab\Registerables
+ * @since 0.7.0
+ */
+
+namespace PinkCrab\Registerables\Registrar;
+
+use Exception;
+use PinkCrab\Registerables\Meta_Data;
+use PinkCrab\Registerables\Registrar\Registrar;
+use PinkCrab\Registerables\Shared_Meta_Box_Controller;
+use PinkCrab\Registerables\Registrar\Meta_Box_Registrar;
+use PinkCrab\Registerables\Registration_Middleware\Registerable;
+
+class Shared_Meta_Box_Registrar implements Registrar {
+
+	/**
+	 * The Meta Box Registrar
+	 *
+	 * @var Meta_Box_Registrar
+	 */
+	protected $meta_box_registrar;
+
+	public function __construct( Meta_Box_Registrar $meta_box_registrar ) {
+		$this->meta_box_registrar = $meta_box_registrar;
+	}
+
+	/**
+	 * Used to register a registerable
+	 *
+	 * @param \PinkCrab\Registerables\Registration_Middleware\Registerable $registerable
+	 * @return void
+	 */
+	public function register( Registerable $registerable ): void {
+		if ( ! is_a( $registerable, Shared_Meta_Box_Controller::class ) ) {
+			return;
+		}
+
+		/** @var Shared_Meta_Box_Controller $registerable, Validation call below catches no Shared_Meta_Box_Controller Registerables */
+
+		// Get the meta box and meta data.
+		$meta_box  = $registerable->meta_box();
+		$meta_data = $registerable->meta_data( array() );
+
+		// Register the meta box.
+		$this->meta_box_registrar->register( $meta_box );
+
+		// Register all meta data.
+		foreach ( $this->filter_meta_data( $meta_data ) as $meta_field ) {
+			// Register meta data for each post type.
+			foreach ( $meta_box->screen as $post_type ) {
+				$this->register_meta_data_for_post_type( $meta_field, $post_type );
+			}
+		}
+
+	}
+
+	/**
+	 * Filters all non meta data from array.
+	 *
+	 * @param mixed[] $meta_data
+	 * @return Meta_Data[]
+	 */
+	protected function filter_meta_data( array $meta_data ): array {
+		return array_filter(
+			$meta_data,
+			function( $e ) {
+				return is_a( $e, Meta_Data::class );
+			}
+		);
+	}
+
+	/**
+	 * Registers the Meta Data for a give post type.
+	 *
+	 * @param Meta_Data $meta_data
+	 * @param string $post_type
+	 * @return void
+	 */
+	protected function register_meta_data_for_post_type( Meta_Data $meta_data, string $post_type ): void {
+
+		// Clone and set the post type, while enforcing it as a post meta.
+		$meta_data = clone $meta_data;
+		$meta_data->object_subtype( $post_type );
+		$meta_data->meta_type( 'post' );
+		$result = register_meta( $meta_data->get_meta_type(), $meta_data->get_meta_key(), $meta_data->parse_args() );
+		if ( ! $result ) {
+			throw new \Exception( "Failed to register {$meta_data->get_meta_key()} (meta) for {$post_type} post type" );
+		}
+	}
+
+
+}
---
+++ tests/Unit/Metaboxes/Test_Shared_Meta_Box_Controller.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Unit tests for the Shared Meta Box Controller
+ *
+ * @since 0.7.0
+ * @author Glynn Quelch <glynn.quelch@gmail.com>
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @package PinkCrab\Perique
+ */
+
+namespace PinkCrab\Registerables\Tests\Unit\Metaboxes;
+
+use WP_UnitTestCase;
+use Gin0115\WPUnit_Helpers\Objects;
+use PinkCrab\Registerables\Meta_Box;
+use PinkCrab\Registerables\Meta_Data;
+use PinkCrab\Registerables\Registrar\Shared_Meta_Box_Registrar;
+use PinkCrab\Registerables\Tests\Fixtures\Shared_Metabox\Post_Page_Meta_Box;
+
+class Test_Shared_Meta_Box_Controller extends WP_UnitTestCase {
+
+	/** @testdox A populated Shared_Meta_Box_Controller should return a populated meta box */
+	public function test_can_get_meta_box(): void {
+		$meta_box = ( new Post_Page_Meta_Box() )->meta_box();
+
+		$this->assertInstanceof( Meta_Box::class, $meta_box );
+
+		$this->assertEquals( 'post_page_mb', $meta_box->key );
+		$this->assertEquals( 'Post and Page Meta Box', $meta_box->label );
+		$this->assertContains( 'post', $meta_box->screen );
+		$this->assertContains( 'page', $meta_box->screen );
+	}
+
+    /** @testdox A populated Shared_Meta_Box_Controller should return an array of populated Meta Data. */
+	public function test_can_get_meta_data(): void {
+		$controller = new Post_Page_Meta_Box();
+		$meta_data  = Objects::invoke_method( $controller, 'meta_data', array( array() ) );
+
+		$this->assertCount( 2, $meta_data );
+
+		$this->assertInstanceOf( Meta_Data::class, $meta_data[0] );
+		$this->assertContains( 'post', $meta_data[0]->get_meta_type() );
+		$this->assertContains( 'pnp_string', $meta_data[0]->get_meta_key() );
+
+		$this->assertInstanceOf( Meta_Data::class, $meta_data[1] );
+		$this->assertContains( 'post', $meta_data[1]->get_meta_type() );
+		$this->assertContains( 'pnp_number', $meta_data[1]->get_meta_key() );
+	}
+}
